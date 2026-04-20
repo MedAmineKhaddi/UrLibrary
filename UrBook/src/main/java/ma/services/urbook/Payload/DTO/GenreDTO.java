68fbe005721e03f3a5700e28bbc5d43d7e4d1641
Amine <medAmineKhaddi@gmail.com>
2026-04-20 20:55:48 +0100

    create GenreDTO class

--- UrBook/src/main/java/ma/services/urbook/Payload/DTO/GenreDTO.java
+++ UrBook/src/main/java/ma/services/urbook/Payload/DTO/GenreDTO.java
@@ -1,10 +1,43 @@
 package ma.services.urbook.Payload.DTO;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
 
 
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class GenreDTO {
 
+    private Long id;
+
+    @NotBlank(message = "Genre Code is Mandatory")
+    private String code;
+
+    @NotBlank(message = "Genre Name is Mandatory")
+    private String name;
+
+    @Size(max=500, message = "display order can not be negative")
+    private String description;
+
+    @Min(value = 0, message = "display order can not be negative")
+    private Integer displayOrder=0;
+
+    private boolean active;
+
+    private Long parentGenreId;
+
+    private String parentGenreName;
+
+    private List<GenreDTO> subGenre;
+
+    private Long bookCount;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 
 }
