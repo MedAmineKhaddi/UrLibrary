72a468848d72bdae2d07f07e0c562f4ff3cac3e7
Amine <medAmineKhaddi@gmail.com>
2026-04-20 19:59:50 +0100

    create Genre model

---
+++ UrBook/src/main/java/ma/services/urbook/Models/Genre.java
@@ -0,0 +1,43 @@
+package ma.services.urbook.Models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.hibernate.annotations.*;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+@Entity @AllArgsConstructor @NoArgsConstructor @Data @Builder
+public class Genre {
+
+    @Id @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank(message = "Genre Code is Mandatory")
+    private String code;
+
+    @NotBlank(message = "Genre Name is Mandatory")
+    private String name;
+
+    @Size(max=500, message = "display order can not be negative")
+    private String description;
+
+    @Min(value = 0, message = "display order can not be negative")
+    private Integer displayOrder=0;
+
+    @Column(nullable = false)
+    private Boolean active=true;
+
+    @ManyToOne
+    private Genre parentGenre;
+
+    @OneToMany
+    private List<Genre> subGenre=new ArrayList<Genre>();
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
