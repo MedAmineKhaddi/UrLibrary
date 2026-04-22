d9683462c22b6be3f6b85252cbbf22a75e4452d4
Amine <medAmineKhaddi@gmail.com>
2026-04-22 21:06:51 +0100

    add methods, implemetation of one method (getGenreById)

--- UrBook/src/main/java/ma/services/urbook/services/GenreService.java
+++ UrBook/src/main/java/ma/services/urbook/services/GenreService.java
@@ -1,13 +1,26 @@
 package ma.services.urbook.services;
 
+import ma.services.urbook.Exceptions.GenreException;
 import ma.services.urbook.Models.Genre;
 import ma.services.urbook.Payload.DTO.GenreDTO;
+import org.springframework.data.domain.Page;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 
 public interface GenreService {
     GenreDTO createGenre(GenreDTO Genre);
-
     List<GenreDTO> getAllGenres();
+    GenreDTO getGenreById(Long id) throws GenreException;
+
+    GenreDTO updateGenre(Long genreId, GenreDTO GenreDTO);
+     void deleteGenre(Long genreId);
+    void hardDeleteGenre(Long genreId);
+    //List<GenreDTO> getAllActiveGenresWithSubGenres();
+    //List<GenreDTO> getTopLevelGenres();
+    //Page<GenreDTO> searchGenres(String searchTerm, Pageable pageable);
+    //Long getTotalActiveGenres();
+    //Long getBookCountByGenres(Long genreId);
+
 }
--- UrBook/src/main/java/ma/services/urbook/services/impl/GenreServiceImpl.java
+++ UrBook/src/main/java/ma/services/urbook/services/impl/GenreServiceImpl.java
@@ -1,14 +1,13 @@
 package ma.services.urbook.services.impl;
 
 import lombok.RequiredArgsConstructor;
+import ma.services.urbook.Exceptions.GenreException;
 import ma.services.urbook.Mappers.GenreMapper;
 import ma.services.urbook.Payload.DTO.GenreDTO;
 import ma.services.urbook.Models.Genre;
 import ma.services.urbook.Repositories.GenreRepository;
 import ma.services.urbook.services.GenreService;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,5 +29,29 @@ public class GenreServiceImpl implements GenreService {
     @Override
     public List<GenreDTO> getAllGenres() {
         return genreRepository.findAll().stream().map(genreMapper::toDTO).collect(Collectors.toList());
+    }
+
+    //add it
+    @Override
+    public GenreDTO getGenreById(Long id) throws GenreException {
+        Genre genre = genreRepository.findById(id).orElseThrow(() -> new GenreException("Genre not found"));
+        GenreDTO dto = genreMapper.toDTO(genre);
+        return dto;
+    }
+
+
+    @Override
+    public void hardDeleteGenre(Long genreId) {
+
+    }
+
+    @Override
+    public void deleteGenre(Long genreId) {
+
+    }
+
+    @Override
+    public GenreDTO updateGenre(Long genreId, GenreDTO GenreDTO) {
+        return null;
     }
 }
