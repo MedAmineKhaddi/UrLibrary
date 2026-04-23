e5ad252d5bb54ae3fd2afa3411c958d3f816118b
Amine <medAmineKhaddi@gmail.com>
2026-04-23 21:02:05 +0100

    add functions to GenreRepository

--- UrBook/src/main/java/ma/services/urbook/Repositories/GenreRepository.java
+++ UrBook/src/main/java/ma/services/urbook/Repositories/GenreRepository.java
@@ -7,4 +7,14 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 public interface GenreRepository extends JpaRepository<Genre,Long> {
 
+        /*
+        * findByActiveTrueOrderByDisplayOrderAsc()
+        findByParentGenreIsNullAndActiveTrueOrderByDisplayOrderAsc()
+        findParentGenreIdAndActiveTrueOrderByDisplayOrderAsc(Long ParentGenreId);
+        long countByActiveTrue();
+        @Query("select count(b) from book b where b.genre.id=:genreId")
+        long countBookByGenre(@Param("genreId") Long genreId);
+        *
+        */
+
 }
