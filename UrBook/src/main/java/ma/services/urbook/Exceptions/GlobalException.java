6e821b081cb77af392fba11a3780afe89f404ba4
Amine <medAmineKhaddi@gmail.com>
2026-04-23 21:03:16 +0100

    add handleGenreException: generate badRequests

---
+++ UrBook/src/main/java/ma/services/urbook/Exceptions/GlobalException.java
@@ -0,0 +1,20 @@
+package ma.services.urbook.Exceptions;
+
+import ma.services.urbook.Payload.Response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(GenreException.class)
+    public ResponseEntity<ApiResponse> handleGenreException(GenreException e) {
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ApiResponse(e.getMessage(),false));
+
+    }
+
+}
