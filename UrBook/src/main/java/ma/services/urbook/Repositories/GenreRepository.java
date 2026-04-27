e18deda7d2a12de06170f61603a97769a392f1e4
Amine <medAmineKhaddi@gmail.com>
2026-04-27 21:03:33 +0100

    Uncomment findByParentGenreId and countByActive methods

--- UrBook/src/main/java/ma/services/urbook/Repositories/GenreRepository.java
+++ UrBook/src/main/java/ma/services/urbook/Repositories/GenreRepository.java
@@ -4,14 +4,16 @@ import ma.services.urbook.Models.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 
 public interface GenreRepository extends JpaRepository<Genre,Long> {
 
+        public List<Genre> findByActiveTrueOrderByDisplayOrderAsc();
+        public List<Genre>findByParentGenreIsNullAndActiveTrueOrderByDisplayOrderAsc();
+        public long countByActiveTrue();
         /*
-        * findByActiveTrueOrderByDisplayOrderAsc()
-        findByParentGenreIsNullAndActiveTrueOrderByDisplayOrderAsc()
         findParentGenreIdAndActiveTrueOrderByDisplayOrderAsc(Long ParentGenreId);
-        long countByActiveTrue();
         @Query("select count(b) from book b where b.genre.id=:genreId")
         long countBookByGenre(@Param("genreId") Long genreId);
         *
