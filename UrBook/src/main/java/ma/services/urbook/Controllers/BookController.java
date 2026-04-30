77b2d56b6d745d47021382ba4171d96e271071e5
Amine <medAmineKhaddi@gmail.com>
2026-04-30 20:55:30 +0100

    Add delete method

--- UrBook/src/main/java/ma/services/urbook/Controllers/BookController.java
+++ UrBook/src/main/java/ma/services/urbook/Controllers/BookController.java
@@ -3,7 +3,9 @@ package ma.services.urbook.Controllers;
 import lombok.RequiredArgsConstructor;
 import ma.services.urbook.Exceptions.BookException;
 import ma.services.urbook.Payload.DTO.BookDTO;
+import ma.services.urbook.Payload.Response.ApiResponse;
 import ma.services.urbook.services.BookService;
+import org.aspectj.apache.bcel.Repository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +28,15 @@ public class BookController {
     @GetMapping("/all-books")
     public ResponseEntity<List<BookDTO>> getAllBooks() throws BookException {
         return ResponseEntity.ok(bookService.getBooks());
+    }
+
+    @DeleteMapping("/delete-book/{bookID}")
+    public ResponseEntity<?> deleteBook(@PathVariable  long bookID) throws BookException
+    {
+      bookService.deleteBookById(bookID);
+
+        ApiResponse apiResponse = new ApiResponse("the book deleted successfully",true);
+        return ResponseEntity.ok(apiResponse);
     }
 
 }
