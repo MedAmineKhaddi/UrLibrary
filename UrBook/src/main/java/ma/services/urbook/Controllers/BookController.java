28644db82a9e805ee8a65a0f21663d124e1432e7
Amine <medAmineKhaddi@gmail.com>
2026-05-02 22:41:09 +0100

    Update toEntity method: change param of active attribut

--- UrBook/src/main/java/ma/services/urbook/Controllers/BookController.java
+++ UrBook/src/main/java/ma/services/urbook/Controllers/BookController.java
@@ -1,11 +1,13 @@
 package ma.services.urbook.Controllers;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import ma.services.urbook.Exceptions.BookException;
 import ma.services.urbook.Payload.DTO.BookDTO;
 import ma.services.urbook.Payload.Response.ApiResponse;
 import ma.services.urbook.services.BookService;
 import org.aspectj.apache.bcel.Repository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,29 +16,46 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/books")
-
 public class BookController {
-
 
     private final BookService bookService;
 
     @PostMapping("/create")
-    public ResponseEntity<?> addBook(@RequestBody BookDTO bookDTO) throws BookException {
+    public ResponseEntity<?> addBook(@Valid @RequestBody BookDTO bookDTO) throws BookException {
         return ResponseEntity.ok(bookService.createBook(bookDTO));
     }
 
-    @GetMapping("/all-books")
-    public ResponseEntity<List<BookDTO>> getAllBooks() throws BookException {
-        return ResponseEntity.ok(bookService.getBooks());
+    //change method
+    @PostMapping("/bulk")
+    public ResponseEntity<?> createBooksBulk(@Valid @RequestBody List<BookDTO> bookDTO) throws BookException {
+        List<BookDTO> createdBooks = bookService.createBooksBulk(bookDTO);
+        return ResponseEntity.ok(createdBooks);
     }
+
 
     @DeleteMapping("/delete-book/{bookID}")
     public ResponseEntity<?> deleteBook(@PathVariable  long bookID) throws BookException
     {
-      bookService.deleteBookById(bookID);
+      bookService.deleteBook(bookID);
 
         ApiResponse apiResponse = new ApiResponse("the book deleted successfully",true);
         return ResponseEntity.ok(apiResponse);
+    }
+
+    @GetMapping("{bookId}")
+    public ResponseEntity<?> getBookById(@PathVariable Long bookId) throws BookException {
+        BookDTO bookDTO = bookService.getBookById(bookId);
+        return ResponseEntity.ok(bookDTO);
+    }
+
+    @PutMapping("/{bookId}")
+    public ResponseEntity<?> updateBook(@PathVariable Long bookId, @Valid @RequestBody BookDTO bookDTO) {
+            try{
+                        BookDTO updatedBook = bookService.updateBook(bookDTO, bookId);
+                        return ResponseEntity.ok(updatedBook);
+            }catch (BookException e){
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            }
     }
 
 }
