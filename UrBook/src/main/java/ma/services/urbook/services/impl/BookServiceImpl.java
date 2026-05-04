bc8282d9a909f428691874fc7ff30fd2b1c9c69a
Amine <medAmineKhaddi@gmail.com>
2026-05-04 20:08:10 +0100

    Update BookServiceImpl class : modify the biblio import of pageable

--- UrBook/src/main/java/ma/services/urbook/services/impl/BookServiceImpl.java
+++ UrBook/src/main/java/ma/services/urbook/services/impl/BookServiceImpl.java
@@ -14,7 +14,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.awt.print.Pageable;
+
+import org.springframework.data.domain.Pageable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
